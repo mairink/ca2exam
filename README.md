6e4c453872dc6cb1392ec5255a6a75bfe4f9f75e
mairink <mairinkavanagh@gmail.com>
2014-03-26 21:12:50 +0000

    final commit

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ ca2exam
 =======
 
 This is the code for the aria exam on 25th March,2014
+
+mairink is my github account name
+ca2exam is the repository
+it is deployed at 
+http://secure-badlands-1427.herokuapp.com
+
+
+
--- test/spec/converterSpec.js
+++ test/spec/converterSpec.js
@@ -5,4 +5,16 @@ describe("My Converter", function() {
     it("should be able to deal with strings", function(){
         expect(function() {Converter.convertFromPoundsToKIlo("hello")}).toThrow(new Error("Not a number"))
     })
+	it(“add numbers”, function() {
+	expect(Converter.addTwoNumbers(3,2)).toBe(5);
+	})
+	it(“subtract two numbers”, function() {
+	expect(Converter.subTwoNumbers(3,2)).toBe(1);
+	})
+	 it(“multiply two numbers”, function() {
+	expect(Converter.multiplyTwoNumbers(3,2)).toBe(6);
+	})
+	it(“divide two numbers”, function() {
+	expect(Converter.divideTwoNumbers(6,2)).toBe(3);
+	})
 })
